a5a72f25af3942aa5a719468ec828de99283f841
Mings1027 <mings1027@gmail.com>
2023-02-11 23:52:47 +0900

    2/11SpiderIKController
    이제 다리가 번갈아가면서 움직이긴 한다. 그러나 높이없이 순간이동하듯 움직이기 때문에 개선이 필요하다

--- 3DTowerDefense/Assets/Scripts/InterpolationCurves/SecondOrder.cs
+++ 3DTowerDefense/Assets/Scripts/InterpolationCurves/SecondOrder.cs
@@ -14,40 +14,12 @@ namespace InterpolationCurves
         [SerializeField] private float speed;
         [SerializeField] [Range(0, 1)] private float bounce;
         [SerializeField] private float rotationSpeed;
-        [SerializeField] private AnimationCurve legCurve;
-        [SerializeField] private float stepDistance;
-        [SerializeField] private float stopDistance;
-        [SerializeField] private float stopTimer;
-        [SerializeField] private float stopTime;
-        private float _lerp;
-        private bool _isMoving;
-
-        private void Start()
-        {
-            _lerp = 1;
-        }
 
         private void FixedUpdate()
         {
-            // Check();
-            // if (_isMoving)
-            //     Second();
-            CheckDistance();
-            if (Vector3.Distance(target.position, transform.position) > stepDistance)
-                Second();
+            Second();
         }
 
-        private void CheckDistance()
-        {
-            var distanceFromTarget = Vector3.Distance(target.position, transform.position);
-            if (!_isMoving && distanceFromTarget > stepDistance)
-            {
-                _isMoving = true;
-                _lerp = 0;
-            }
-
-            if (_lerp >= 1) _isMoving = false;
-        }
 
         private void Second()
         {
@@ -55,21 +27,15 @@ namespace InterpolationCurves
             _curPos = Vector3.Lerp(position, target.position, (1 - bounce) * speed * Time.fixedDeltaTime);
             _curPos += bounce * (_curPos - _oldPos);
             _oldPos = position;
-            if (_lerp < 1)
-            {
-                _curPos.y += legCurve.Evaluate(_lerp);
-                _lerp += Time.fixedDeltaTime * speed;
-            }
-
-            transform.position = _curPos;
+            transform.SetPositionAndRotation(_curPos,
+                Quaternion.Slerp(transform.rotation, target.rotation,
+                    (1 - bounce) * rotationSpeed * Time.fixedDeltaTime));
         }
 
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(transform.position, stepDistance);
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, stopDistance);
+            Gizmos.DrawLine(transform.position, target.position);
         }
     }
 }
