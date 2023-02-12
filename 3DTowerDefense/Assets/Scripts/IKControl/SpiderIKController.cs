2441f1ffe23c57747008c89bb5cd625798a80b5a
Mings1027 <mings1027@gmail.com>
2023-02-13 00:50:45 +0900

    2/12IK
    다리마다 쿨타임을 줘서 이쪽 다리가 움직일때 반대다리 쿨타임을 줄여줘야함 그래야 움직임이 끝났을때 반대다리가 움직일 수 있지

--- 3DTowerDefense/Assets/Scripts/IKControl/SpiderIKController.cs
+++ 3DTowerDefense/Assets/Scripts/IKControl/SpiderIKController.cs
@@ -8,21 +8,20 @@ namespace IKControl
     public class SpiderIKController : MonoBehaviour
     {
         private RaycastHit[] _hit;
-        private Vector3 oldBodyPos, curBodyPos, nextBodyPos;
+
+        // private Vector3 oldBodyPos, curBodyPos, nextBodyPos;
         private Vector3[] _oldLegPos, _curLegPos, _nextLegPos;
         private float _lerp;
-        private bool[] _isMoving;
         private int _legIndex;
 
-        [SerializeField] [Range(0, 1)] private float bounce;
-        [SerializeField] private float speed, rotationSpeed;
-        [SerializeField] private Transform body;
+        // [SerializeField] [Range(0, 1)] private float bounce;
+        // [SerializeField] private float speed, rotationSpeed;
+        // [SerializeField] private Transform body;
 
         [SerializeField] private LayerMask walkableLayer;
         [SerializeField] private Transform[] legs;
 
-        [FormerlySerializedAs("_sensor")] [SerializeField]
-        private Transform[] sensor;
+        [SerializeField] private Transform[] sensor;
 
         [SerializeField] private float stepDistance;
         [SerializeField] private float stepTime;
@@ -37,15 +36,13 @@ namespace IKControl
             {
                 _oldLegPos[i] = _curLegPos[i] = _nextLegPos[i] = legs[i].position;
             }
-
-            // _lerp = new float[legs.Length];
-            _isMoving = new bool[legs.Length];
         }
 
         private void FixedUpdate()
         {
             Check();
-            BodyMove();
+            // LegControl();
+            // BodyMove();
         }
 
 
@@ -56,40 +53,47 @@ namespace IKControl
                 legs[i].position = _curLegPos[i];
             }
 
-            if (Physics.Raycast(sensor[_legIndex].position, Vector3.down, out _hit[_legIndex], 100, walkableLayer))
+            if (_lerp > 0)
             {
-                if (Vector3.Distance(_nextLegPos[_legIndex], _hit[_legIndex].point) > stepDistance)
+                if (Physics.Raycast(sensor[_legIndex].position, Vector3.down, out _hit[_legIndex], 100, walkableLayer))
                 {
-                    _lerp = 0;
-                    _oldLegPos[_legIndex] = legs[_legIndex].position;
-                    _nextLegPos[_legIndex] = _hit[_legIndex].point;
+                    if (Vector3.Distance(_nextLegPos[_legIndex], _hit[_legIndex].point) > stepDistance)
+                    {
+                        _lerp = 0;
+                        _oldLegPos[_legIndex] = legs[_legIndex].position;
+                        _nextLegPos[_legIndex] = _hit[_legIndex].point;
+                    }
+                    // else
+                    // {
+                    //     _lerp = 2;
+                    // }
                 }
             }
 
             if (_lerp < 1)
             {
                 _curLegPos[_legIndex] = Vector3.Lerp(_oldLegPos[_legIndex], _nextLegPos[_legIndex], _lerp);
-                _lerp += Time.fixedDeltaTime / stepTime;
+                _lerp += Time.fixedDeltaTime * stepTime;
 
                 // if (body.position != transform.position)
                 //     _curLegPos[_legIndex].y += Mathf.Sin(_lerp * Mathf.PI * stepHeight);
             }
-
-            if (_lerp >= 1)
+            else
+            {
                 _legIndex = (_legIndex + 1) % legs.Length;
+            }
         }
 
-
-        private void BodyMove()
-        {
-            var bodyPos = body.position;
-            curBodyPos = Vector3.Lerp(bodyPos, transform.position, (1 - bounce) * speed * Time.fixedDeltaTime);
-            curBodyPos += bounce * (curBodyPos - oldBodyPos);
-            oldBodyPos = bodyPos;
-            body.SetPositionAndRotation(curBodyPos,
-                Quaternion.Slerp(body.rotation, transform.rotation,
-                    (1 - bounce) * rotationSpeed * Time.fixedDeltaTime));
-        }
+        // private void BodyMove()
+        // {
+        //     var bodyPos = body.position;
+        //     curBodyPos = Vector3.Lerp(bodyPos, transform.position, (1 - bounce) * speed * Time.fixedDeltaTime);
+        //     curBodyPos += bounce * (curBodyPos - oldBodyPos);
+        //     oldBodyPos = bodyPos;
+        //     body.SetPositionAndRotation(curBodyPos,
+        //         Quaternion.Slerp(body.rotation, transform.rotation,
+        //             (1 - bounce) * rotationSpeed * Time.fixedDeltaTime));
+        // }
 
 
         private void OnDrawGizmos()
@@ -102,18 +106,18 @@ namespace IKControl
                     Gizmos.DrawSphere(_nextLegPos[i], 0.1f);
                 }
             }
-
-            Gizmos.color = Color.green;
-            for (var i = 0; i < legs.Length; i++)
-            {
-                Gizmos.DrawSphere(_hit[i].point, 0.1f);
-            }
-
-            Gizmos.color = Color.cyan;
-            for (int i = 0; i < legs.Length; i++)
-            {
-                Gizmos.DrawLine(body.position, legs[i].position);
-            }
+            //
+            // Gizmos.color = Color.green;
+            // for (var i = 0; i < legs.Length; i++)
+            // {
+            //     Gizmos.DrawSphere(_hit[i].point, 0.1f);
+            // }
+            //
+            // Gizmos.color = Color.cyan;
+            // for (int i = 0; i < legs.Length; i++)
+            // {
+            //     Gizmos.DrawLine(body.position, legs[i].position);
+            // }
         }
     }
 }
