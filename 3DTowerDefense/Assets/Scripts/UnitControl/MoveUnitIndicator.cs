2602a96eb5cceaedbd3df7bad9cad8f7a5599988
Mings1027 <forum.06-bandeau@icloud.com>
2023-05-11 09:12:35 +0900

    5/10 UI 오작동 수정함 커밋 여러줄 입력 테스트

---
+++ 3DTowerDefense/Assets/Scripts/UnitControl/MoveUnitIndicator.cs
@@ -0,0 +1,27 @@
+using System;
+using UIControl;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnitControl
+{
+    public class MoveUnitIndicator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        private UIManager _uiManager;
+
+        private void Awake()
+        {
+            _uiManager = UIManager.Instance;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _uiManager.MoveUnit();
+            gameObject.SetActive(false);
+        }
+    }
+}
