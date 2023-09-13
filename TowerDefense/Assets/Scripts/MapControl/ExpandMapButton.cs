931e53bb010eaa1866ea46327e7c699cb7448ab1
Mings1027 <100500113+Mings1027@users.noreply.github.com>
2023-09-14 00:20:47 +0900

    9/13 피드백반영 및 기능 수정 expandButton에도 IOpenUI 상속시킴 => 하지 않으면 버튼에 의해 ExpandMap호출을 못시킴

--- TowerDefense/Assets/Scripts/MapControl/ExpandMapButton.cs
+++ TowerDefense/Assets/Scripts/MapControl/ExpandMapButton.cs
@@ -1,30 +1,26 @@
 using System;
+using InterfaceControl;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace MapControl
 {
-    public class ExpandMapButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    public class ExpandMapButton : MonoBehaviour, IOpenUI
     {
         public event Action<Vector3> OnExpandMapEvent;
 
-        public void OnPointerDown(PointerEventData eventData)
-        {
-        }
-
-        public void OnPointerUp(PointerEventData eventData)
-        {
-            OnExpandMapEvent?.Invoke(transform.position);
-        }
-
-        public void Expand()
-        {
-            OnExpandMapEvent?.Invoke(transform.position);
-        }
+        // public void Expand()
+        // {
+        //     OnExpandMapEvent?.Invoke(transform.position);
+        // }
 
         private void OnDisable()
         {
             OnExpandMapEvent = null;
+        }
+
+        public void OpenUI()
+        {
+            OnExpandMapEvent?.Invoke(transform.position);
         }
     }
 }
